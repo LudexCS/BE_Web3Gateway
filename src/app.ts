febf7ba12ff87dd7608330278b853e9df6070095
Gomdadi <35wnsgh@naver.com>
2025-04-19 02:26:54 +0900

    feat: CORS 의존성 추가 + open(개발용)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import authRoute from './route/wallet_auth.route';
 import jwtGuard from './middleware/jwt.guard';
+import cors from 'cors';
 
 const app : Express = express();
 app.use(express.json());
+app.use(cors());
 
 // middleware
 app.use('/api/protected', jwtGuard);
