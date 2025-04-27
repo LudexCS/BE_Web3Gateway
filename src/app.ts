b6f3c01d6c6653f69148fabe6d4ab58d96611528
Gomdadi <35wnsgh@naver.com>
2025-04-27 23:21:00 +0900

    feat: swagger UI 설정

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Express } from 'express';
 import authRoute from './route/wallet_auth.route';
 import jwtGuard from './middleware/jwt.guard';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { specs, swaggerUiOptions } from './config/swagger.config';
 
 const app : Express = express();
 app.use(express.json());
@@ -9,6 +11,9 @@ app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true,
 }));
+
+// Swagger UI 설정
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, swaggerUiOptions));
 
 // middleware
 app.use('/api/protected', jwtGuard);
