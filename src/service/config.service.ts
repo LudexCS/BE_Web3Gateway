a61f483735ca9d604dc3a01599a8ec0ce26ec194
Gomdadi <35wnsgh@naver.com>
2025-05-12 16:10:48 +0900

    fix: Update RPC URL handling and switch to public chain config
    Replaced hardcoded RPC URL with environment variable for better flexibility. Updated configuration logic to use `createPublicChainConfig` instead of `createPrivateChainConfig` for public chain compatibility. These changes improve adaptability and support for external configurations.

--- src/service/config.service.ts
+++ src/service/config.service.ts
@@ -1,7 +1,11 @@
-import {createLudexConfig, createPrivateChainConfig, getContracts} from "../config/ludex.config";
+import {
+    createLudexConfig,
+    createPublicChainConfig,
+    getContracts
+} from "../config/ludex.config";
 
 export function getConfig() {
-    const chainConfig = createPrivateChainConfig();
+    const chainConfig = createPublicChainConfig();
     const contracts = getContracts();
     const ludexConfig = createLudexConfig(contracts);
     return {chainConfig, ludexConfig};
