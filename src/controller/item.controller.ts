87149bcd0d39656b4de5c68f9de79af258e71fdd
Gomdadi <35wnsgh@naver.com>
2025-05-13 15:15:28 +0900

    refactor: Convert bigint to string for consistent Web3 ID handling
    Updated itemId and sharerIds to be passed as strings instead of bigints when storing Web3 IDs. This ensures compatibility with the `storeId.client` implementation and eliminates unnecessary conversions.

--- src/controller/item.controller.ts
+++ src/controller/item.controller.ts
@@ -5,7 +5,9 @@ import {storeWeb3Id} from "../grpc/storeId.client";
 export async function registerItemControl(itemDto: RegisterItemDto) {
     const gameId = itemDto.gameId;
     const { itemId, sharerIds } = await registerItem(itemDto);
-    const success = await storeWeb3Id(gameId, itemId, sharerIds);
+    const stringItemId = itemId.toString();
+    const stringSharerIds = sharerIds.map(id => id.toString());
+    const success = await storeWeb3Id(gameId, stringItemId, stringSharerIds);
     if (!success) {
         console.log("Failed to store web3Id");
     }
